63ab58fd653adaf97e8a9da50f00a972f80bb1b1
Priyanshu5145 <agraharipriyanshu53@gmail.com>
2025-06-14 17:27:00 +0530

    change sns parrser

--- routes/sns.route.js
+++ routes/sns.route.js
@@ -1,12 +1,14 @@
-// routes/sns.route.js
 import express from "express";
 import { handleSnsEvent } from "../controllers/sns.controller.js";
+import bodyParser from "body-parser";
 
 const router = express.Router();
 
-// Make sure to use JSON parser for SNS
-router.use(express.json({ type: "application/json" }));
-
-router.post("/aws/sns-events", handleSnsEvent);
+// Use raw parser for SNS to avoid parsing issues
+router.post(
+  "/aws/sns-events",
+  bodyParser.raw({ type: "application/json" }),
+  handleSnsEvent
+);
 
 export default router;
