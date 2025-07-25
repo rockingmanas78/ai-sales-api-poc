bcc93910a93eca7ec047ab4f812595ce4e6924ef
Manas Srivastava <rockingmanas78@gmail.com>
2025-07-25 18:40:11 +0530

    subscription bug fixed

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -218,13 +218,6 @@ export const verifyPhonePePaymentStatus = async (req, res) => {
 
   try {
     const status = await verifyPhonePeStatus(orderId);
-
-    if (!status || typeof status !== 'object') {
-      return res.status(502).json({ message: 'Invalid response from PhonePe' });
-    }
-
-    const state = status.state;
-
     // console.log(status);
 
     if (!status || typeof status !== 'object') {
