c77d5fb5a33abaadab74b669fa1e8a5142f5ee5b
Sarthak-Shreshtha01 <sarthakshreshtha345@gmail.com>
2025-10-01 12:20:19 +0530

    Get Conversation by Campaign ID

--- routes/conversation.route.js
+++ routes/conversation.route.js
@@ -1,20 +1,13 @@
 // routes/conversation.routes.js
-import express from "express";
-import verifyToken from "../middlewares/verifyToken.js";
-import authorize from "../middlewares/rbac.js";
-import {
-  getConversationMessages,
-  listConversations,
-} from "../controllers/conversations.controller.js";
+import express from 'express';
+import verifyToken from '../middlewares/verifyToken.js';
+import authorize from '../middlewares/rbac.js';
+import { getConversationMessages, listConversations ,getUniqueConversationsByCampaignID} from '../controllers/conversations.controller.js';
 
 const router = express.Router();
 
-router.get("/", verifyToken(), authorize("view_emails"), listConversations);
-router.get(
-  "/:conversationId/messages",
-  verifyToken(),
-  authorize("view_emails"),
-  getConversationMessages
-);
+router.get('/', verifyToken(), authorize('view_emails'), listConversations);
+router.get('/:conversationId/messages', verifyToken(), authorize('view_emails'), getConversationMessages);
+router.get('/campaign/:campaignId', verifyToken(), authorize('view_emails'), getUniqueConversationsByCampaignID);
 
 export default router;
