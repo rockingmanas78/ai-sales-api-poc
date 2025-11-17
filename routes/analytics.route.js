a57125e2d3a23d5f2520bcf38cb4c4002e90997a
urvashi2004 <urvashi214btcse23@igdtuw.ac.in>
2025-11-18 01:47:25 +0530

    Analytics

--- routes/analytics.route.js
+++ routes/analytics.route.js
@@ -1,11 +1,15 @@
-import express from 'express';
-import { getAnalyticsOverview, getViewUsage } from '../controllers/analytics.controller.js';
-import verifyToken from '../middlewares/verifyToken.js';
-
+import express from "express";
+import {
+  getAnalyticsOverview,
+  getViewUsage,
+  getHealthDeliverability,
+} from "../controllers/analytics.controller.js";
+import verifyToken from "../middlewares/verifyToken.js";
 
 const router = express.Router();
 
-router.get('/overview', verifyToken(), getAnalyticsOverview);
-router.get('/view-usage', verifyToken() , getViewUsage );
+router.get("/overview", verifyToken(), getAnalyticsOverview);
+router.get("/view-usage", verifyToken(), getViewUsage);
+router.get("/health-deliverability", verifyToken(), getHealthDeliverability);
 
 export default router;
--- routes/bulkEmail.routes.js
+++ routes/bulkEmail.routes.js
@@ -4,18 +4,45 @@ import {
   getBulkEmailJobs,
   getBulkEmailJobById,
   pauseBulkEmailJob,
-  resumeBulkEmailJob
+  resumeBulkEmailJob,
 } from "../controllers/bulkEmail.controller.js";
-
 import verifyToken from "../middlewares/verifyToken.js";
 import authorize from "../middlewares/rbac.js";
+import { checkEventUsageLimits } from "../middlewares/usage.js";
+import { MeterMetric } from "@prisma/client";
 
 const router = Router();
-router.post("/bulk-send",verifyToken(), authorize('manage_emails'), createBulkEmailJob);
-router.get("/jobs/:jobId",verifyToken(),authorize('view_emails'), getBulkEmailJobById);
-router.get("/jobs/:tenantId",verifyToken(),authorize('view_emails'), getBulkEmailJobs);
-router.post("/jobs/:jobId/pause",verifyToken(),authorize('manage_emails'), pauseBulkEmailJob);
-router.post("/jobs/:jobId/resume",verifyToken(),authorize('manage_emails'), resumeBulkEmailJob);
+router.post(
+  "/bulk-send",
+  verifyToken(),
+  authorize("manage_emails"),
+  checkEventUsageLimits(MeterMetric.EMAILS),
+  createBulkEmailJob
+);
+router.get(
+  "/jobs/:jobId",
+  verifyToken(),
+  authorize("view_emails"),
+  getBulkEmailJobById
+);
+router.get(
+  "/jobs/:tenantId",
+  verifyToken(),
+  authorize("view_emails"),
+  getBulkEmailJobs
+);
+router.post(
+  "/jobs/:jobId/pause",
+  verifyToken(),
+  authorize("manage_emails"),
+  pauseBulkEmailJob
+);
+router.post(
+  "/jobs/:jobId/resume",
+  verifyToken(),
+  authorize("manage_emails"),
+  resumeBulkEmailJob
+);
 export default router;
 
 // import { Router } from "express";
